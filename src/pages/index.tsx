0d6f85b952332ce9430cd322e8db2bcb70395319
Apple <appleteng97@gmail.com>
2023-03-29 23:59:29 +0800

    refactor: Migrate to nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home(){
+  return (
+    <>
+      <Head>
+        <title>Expense Tracker</title>
+      </Head>
+      <main>
+        <h1>Expense Tracker</h1>
+      </main>
+    </>
+  )
+}
