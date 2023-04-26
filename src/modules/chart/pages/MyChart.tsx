5fdceb10403c03d7fcc86c283b0eedf520efcfc1
Apple <appleteng97@gmail.com>
2023-04-27 02:28:32 +0800

    refactor: Update MyChart page
    This commit update the code to:
    - Rename the props from chartData to transactionDetails
    - Pass the transactions details to Card component, and process the card in Card component instead of getting the chart data by calling the custom hooks and pass to the component

--- src/modules/chart/pages/MyChart.tsx
+++ src/modules/chart/pages/MyChart.tsx
@@ -2,24 +2,22 @@ import { Chart } from "../components/Chart"
 import { ChartProps } from "../../../types/TransactionInterfaceType"
 import { getChartDataApiService } from "../services/GetChartDataApiService"
 import { OthersMenuBar } from "../../../components/OtherMenubar"
-import { useProcessChartData } from "../hooks/useProcessChartData"
 
 export const getServerSideProps = async () =>{
     const response = await getChartDataApiService.getChartData()
     return{
         props:{
-            chartData:response.data
+            transactionsDetails:response.data
         }
     }
 }
 
-export const MyChart = ({ chartData } : ChartProps) =>{
-
-    const { expenseChartData, incomeChartData, detailsData } = useProcessChartData(chartData)
+export const MyChart = ({ transactionsDetails } : ChartProps) =>{
+    
     return(
         <>
             <OthersMenuBar />
-            <Chart expenseChartData={expenseChartData} incomeChartData={incomeChartData} detailsData={detailsData}/>
+            <Chart transactionsDetails={transactionsDetails} />
         </>
     )
 }
