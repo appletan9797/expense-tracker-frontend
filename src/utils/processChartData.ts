1298d32de32a4ac566f0d4bff88bcddee925871b
Apple <appleteng97@gmail.com>
2023-04-27 02:46:37 +0800

    feat: Add a processChartData util to process transactions to chart required format

---
+++ src/utils/processChartData.ts
@@ -0,0 +1,31 @@
+import { ChartProps, TransactionDetails } from "../types/TransactionInterfaceType"
+import _ from "lodash"
+
+class ProcessChartDataUtils{
+
+    processChartData(transactionDetails:TransactionDetails[], currencyId: number, transactionType:string){
+        const groupedData = this.getGroupedData(transactionDetails, currencyId,transactionType)
+        return this.getPieChartData(groupedData)
+    }
+    
+    getGroupedData(transactionDetails:TransactionDetails[], currencyId: number, transactionType:string){
+        const filteredTransaction = _.filter(transactionDetails, (eachTransaction) => 
+                                                        eachTransaction.currency_id === currencyId && 
+                                                        eachTransaction.transaction_type === transactionType)                                  
+        const groupedData = _.groupBy(filteredTransaction, (eachTransaction) => eachTransaction.category_name_en)
+        return groupedData
+    }
+
+    getPieChartData(data:ChartProps){
+        const totalAmount = _.sumBy(_.flatten((Object.values(data))), transaction => Number(transaction.transaction_amount))
+        const res = _.map(data, (eachCategory, categoryName) =>{
+            return{
+                "label":categoryName,
+                "value":((_.sumBy(eachCategory,(transaction) => Number(transaction.transaction_amount))/ totalAmount) * 100).toFixed(1)
+            }
+        })
+        return res
+    }
+}
+
+export const processChartDataUtils = new ProcessChartDataUtils()
